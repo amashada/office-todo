95e2bcce79f3d92176da646c27ed656b210dbf3a
Amashada Navoda <amashadanavoda@Amashadas-MacBook-Air.local>
2026-09-18 15:10:23 +0530

    Added Firebase Firestore Cloud Integration

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDb2KMSmdPwJqDiDEMtiE9qSvljZvuf9zI",
+  authDomain: "office-todo-b55f8.firebaseapp.com",
+  projectId: "office-todo-b55f8",
+  storageBucket: "office-todo-b55f8.firebasestorage.app",
+  messagingSenderId: "586831347498",
+  appId: "1:586831347498:web:ed27ae2373fe59e088e853",
+  measurementId: "G-YTTCXC0ND3"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
